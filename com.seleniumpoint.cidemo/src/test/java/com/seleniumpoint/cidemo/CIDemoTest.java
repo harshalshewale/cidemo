38cf2f26024221ef1ac4b2181cfcaf280e92acc6
Harshal_Shewale <Harshal_Shewale@SIA700054LX.sq.com.sg>
2018-03-28 22:33:30 +0800

    First Commit

---
+++ com.seleniumpoint.cidemo/src/test/java/com/seleniumpoint/cidemo/CIDemoTest.java
@@ -0,0 +1,14 @@
+package com.seleniumpoint.cidemo;
+
+import org.testng.annotations.*;
+
+public class CIDemoTest {
+
+	@Test
+	public void printHello()
+	{
+		System.out.println("Hello SeleniumPoint");
+	}
+	
+	
+}
